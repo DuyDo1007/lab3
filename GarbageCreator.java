4a20031fc3dad182a1ffbd571b693e0870b2b3ac
DuyDo1007 <165807434+DuyDo1007@users.noreply.github.com>
2024-11-26 17:11:52 +0700

    Add String, StringBufer & StringBuilder exercise

---
+++ GarbageCreator.java
@@ -0,0 +1,17 @@
+package hust.soict.dsai.garbage;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class GarbageCreator {
+    public static void main(String[] args) throws Exception {
+        byte[] inputBytes = Files.readAllBytes(Paths.get("test.txt"));
+        long startTime = System.currentTimeMillis();
+        String outputString = "";
+        for (byte b : inputBytes) {
+            outputString += (char) b;
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println(endTime - startTime);
+    }
+}
